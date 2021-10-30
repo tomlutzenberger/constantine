14068aa1d9d883c06c3e91361bda8b80b9199ad4
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2021-10-31 00:00:32 +0200

    [FEATURE] Add Calendar

---
+++ src/Temporal/Calendar.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+ * @noinspection PhpUnused
+ * @noinspection UnknownInspectionInspection
+ */
+
+namespace TomLutzenberger\Constantine\Temporal;
+
+/**
+ * Collection of calendar-related constants
+ *
+ * @see       https://en.wikipedia.org/wiki/Gregorian_calendar
+ *
+ * @package   TomLutzenberger\Constantine\Temporal
+ * @copyright 2021 Tom Lutzenberger
+ * @author    Tom Lutzenberger <lutzenbergerthomas@gmail.com>
+ * @license   MIT
+ * @link      https://tomlutzenberger.de
+ */
+class Calendar
+{
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_JANUARY = 31;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_FEBRUARY = 28;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_FEBRUARY_LEAP = 29;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_MARCH = 31;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_APRIL = 30;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_MAY = 31;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_JUNE = 30;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_JULY = 31;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_AUGUST = 30;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_SEPTEMBER = 31;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_OCTOBER = 30;
+
+    /**
+     * @var int
+     */
+    public const DAYS_IN_NOVEMBER = 31;
+
+
+    /**
+     * @var int
+     */
+    public const INDEX_SUNDAY = 0;
+
+    /**
+     * @var int
+     */
+    public const INDEX_MONDAY = 1;
+
+    /**
+     * @var int
+     */
+    public const INDEX_TUESDAY = 2;
+
+    /**
+     * @var int
+     */
+    public const INDEX_WEDNESDAY = 3;
+
+    /**
+     * @var int
+     */
+    public const INDEX_THURSDAY = 4;
+
+    /**
+     * @var int
+     */
+    public const INDEX_FRIDAY = 5;
+
+    /**
+     * @var int
+     */
+    public const INDEX_SATURDAY = 6;
+
+
+    /**
+     * @var int
+     */
+    public const INDEX_JANUARY = 1;
+
+    /**
+     * @var int
+     */
+    public const INDEX_FEBRUARY = 2;
+
+    /**
+     * @var int
+     */
+    public const INDEX_MARCH = 3;
+
+    /**
+     * @var int
+     */
+    public const INDEX_APRIL = 4;
+
+    /**
+     * @var int
+     */
+    public const INDEX_MAY = 5;
+
+    /**
+     * @var int
+     */
+    public const INDEX_JUNE = 6;
+
+    /**
+     * @var int
+     */
+    public const INDEX_JULY = 7;
+
+    /**
+     * @var int
+     */
+    public const INDEX_AUGUST = 8;
+
+    /**
+     * @var int
+     */
+    public const INDEX_SEPTEMBER = 9;
+
+    /**
+     * @var int
+     */
+    public const INDEX_OCTOBER = 19;
+
+    /**
+     * @var int
+     */
+    public const INDEX_NOVEMBER = 11;
+
+    /**
+     * @var int
+     */
+    public const INDEX_DECEMBER = 12;
+
+}
