2419fc79d87d50d91c12dc2642f21d065709f6c0
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2021-12-14 23:05:59 +0100

    [FEATURE] Add World Geodetic System (WGS84)

---
+++ src/Geography/WorldGeodeticSystem.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @noinspection PhpUnused
+ * @noinspection UnknownInspectionInspection
+ */
+
+namespace TomLutzenberger\Constantine\Geography;
+
+/**
+ * Collection of Geodetic constants (currently WGS84)
+ *
+ * @see       https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+ * @see       https://www.unoosa.org/pdf/icg/2012/template/WGS_84.pdf
+ *
+ * @package   TomLutzenberger\Constantine\Geography
+ * @copyright 2021 Tom Lutzenberger
+ * @author    Tom Lutzenberger <lutzenbergerthomas@gmail.com>
+ * @license   MIT
+ */
+class WorldGeodeticSystem
+{
+
+    /**
+     * Semi-major Axis
+     *
+     * Symbol: a
+     * Unit:  m (meters)
+     *
+     * @var float
+     */
+    public const SEMI_MAJOR_AXIS = 6378137.0;
+
+    /**
+     *Semi-minor axis
+     *
+     * Symbol: b
+     * Unit: m (meters)
+     *
+     * @var float
+     */
+    public const SEMI_MINOR_AXIS = 6356752.314245;
+
+    /**
+     * Flattening Factor of the Earth
+     *
+     * Symbol: 1/f
+     * Unit:
+     *
+     * @var float
+     */
+    public const FLATTENING_FACTOR = 298.257223563;
+
+    /**
+     * Nominal Mean Angular Velocity of the Earth
+     *
+     * Symbol: ω
+     * Unit:  radians/second
+     *
+     * @var float
+     */
+    public const NOMINAL_MEAN_ANGULAR_VELOCITY = 7292115 * 10E-11;
+
+    /**
+     * Geocentric Gravitational Constant (Mass of Earth’s Atmosphere Included)
+     *
+     * Symbol: GM
+     * Unit:  meter³/second²
+     *
+     * @var float
+     */
+    public const GEOCENTRIC_GRAVITATIONAL_CONSTANT = 3.986004418 * 10E14;
+}
