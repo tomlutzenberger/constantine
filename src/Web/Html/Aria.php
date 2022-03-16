f12d5254e91bf3cb7262a019147ed921fbe03a1e
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2022-03-16 22:23:11 +0100

    [FEATURE] Add HTML aria attributes

---
+++ src/Web/Html/Aria.php
@@ -0,0 +1,563 @@
+<?php
+
+/**
+ * @noinspection PhpUnused
+ * @noinspection UnknownInspectionInspection
+ * @noinspection PhpConstantNamingConventionInspection
+ */
+
+namespace TomLutzenberger\Constantine\Web\Html;
+
+/**
+ * Collection of HTML aria attributes
+ *
+ * @see       https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes
+ *
+ * @package   TomLutzenberger\Constantine\Web\Html
+ * @copyright 2022 Tom Lutzenberger
+ * @author    Tom Lutzenberger <lutzenbergerthomas@gmail.com>
+ * @license   MIT
+ * @link      https://tomlutzenberger.de
+ */
+class Aria
+{
+    /**
+     * HTML aria attribute 'aria-autocomplete'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-autocomplete
+     */
+    public const ARIA_AUTOCOMPLETE = 'aria-autocomplete';
+
+
+    /**
+     * HTML aria attribute 'aria-checked'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-checked
+     */
+    public const ARIA_CHECKED = 'aria-checked';
+
+
+    /**
+     * HTML aria attribute 'aria-disabled'
+     *
+     * Type: Widget attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-disabled
+     */
+    public const ARIA_DISABLED = 'aria-disabled';
+
+
+    /**
+     * HTML aria attribute 'aria-errormessage'
+     *
+     * Type: Widget attributes, Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-errormessage
+     */
+    public const ARIA_ERRORMESSAGE = 'aria-errormessage';
+
+
+    /**
+     * HTML aria attribute 'aria-expanded'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-expanded
+     */
+    public const ARIA_EXPANDED = 'aria-expanded';
+
+
+    /**
+     * HTML aria attribute 'aria-haspopup'
+     *
+     * Type: Widget attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-haspopup
+     */
+    public const ARIA_HASPOPUP = 'aria-haspopup';
+
+
+    /**
+     * HTML aria attribute 'aria-hidden'
+     *
+     * Type: Widget attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden
+     */
+    public const ARIA_HIDDEN = 'aria-hidden';
+
+
+    /**
+     * HTML aria attribute 'aria-invalid'
+     *
+     * Type: Widget attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-invalid
+     */
+    public const ARIA_INVALID = 'aria-invalid';
+
+
+    /**
+     * HTML aria attribute 'aria-label'
+     *
+     * Type: Widget attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-label
+     */
+    public const ARIA_LABEL = 'aria-label';
+
+
+    /**
+     * HTML aria attribute 'aria-level'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-level
+     */
+    public const ARIA_LEVEL = 'aria-level';
+
+
+    /**
+     * HTML aria attribute 'aria-modal'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-modal
+     */
+    public const ARIA_MODAL = 'aria-modal';
+
+
+    /**
+     * HTML aria attribute 'aria-multiline'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-multiline
+     */
+    public const ARIA_MULTILINE = 'aria-multiline';
+
+
+    /**
+     * HTML aria attribute 'aria-multiselectable'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-multiselectable
+     */
+    public const ARIA_MULTISELECTABLE = 'aria-multiselectable';
+
+
+    /**
+     * HTML aria attribute 'aria-orientation'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-orientation
+     */
+    public const ARIA_ORIENTATION = 'aria-orientation';
+
+
+    /**
+     * HTML aria attribute 'aria-placeholder'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-placeholder
+     */
+    public const ARIA_PLACEHOLDER = 'aria-placeholder';
+
+
+    /**
+     * HTML aria attribute 'aria-pressed'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-pressed
+     */
+    public const ARIA_PRESSED = 'aria-pressed';
+
+
+    /**
+     * HTML aria attribute 'aria-readonly'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-readonly
+     */
+    public const ARIA_READONLY = 'aria-readonly';
+
+
+    /**
+     * HTML aria attribute 'aria-required'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-required
+     */
+    public const ARIA_REQUIRED = 'aria-required';
+
+
+    /**
+     * HTML aria attribute 'aria-selected'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-selected
+     */
+    public const ARIA_SELECTED = 'aria-selected';
+
+
+    /**
+     * HTML aria attribute 'aria-sort'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-sort
+     */
+    public const ARIA_SORT = 'aria-sort';
+
+
+    /**
+     * HTML aria attribute 'aria-valuemax'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-valuemax
+     */
+    public const ARIA_VALUEMAX = 'aria-valuemax';
+
+
+    /**
+     * HTML aria attribute 'aria-valuemin'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-valuemin
+     */
+    public const ARIA_VALUEMIN = 'aria-valuemin';
+
+
+    /**
+     * HTML aria attribute 'aria-valuenow'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-valuenow
+     */
+    public const ARIA_VALUENOW = 'aria-valuenow';
+
+
+    /**
+     * HTML aria attribute 'aria-valuetext'
+     *
+     * Type: Widget attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-valuetext
+     */
+    public const ARIA_VALUETEXT = 'aria-valuetext';
+
+
+    /**
+     * HTML aria attribute 'aria-busy'
+     *
+     * Type: Live region attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-busy
+     */
+    public const ARIA_BUSY = 'aria-busy';
+
+
+    /**
+     * HTML aria attribute 'aria-live'
+     *
+     * Type: Live region attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-live
+     */
+    public const ARIA_LIVE = 'aria-live';
+
+
+    /**
+     * HTML aria attribute 'aria-relevant'
+     *
+     * Type: Live region attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-relevant
+     */
+    public const ARIA_RELEVANT = 'aria-relevant';
+
+
+    /**
+     * HTML aria attribute 'aria-atomic'
+     *
+     * Type: Live region attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-atomic
+     */
+    public const ARIA_ATOMIC = 'aria-atomic';
+
+
+    /**
+     * HTML aria attribute 'aria-dropeffect'
+     *
+     * Type: Drag-and-Drop attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-dropeffect
+     * @deprecated
+     */
+    public const ARIA_DROPEFFECT = 'aria-dropeffect';
+
+
+    /**
+     * HTML aria attribute 'aria-grabbed'
+     *
+     * Type: Drag-and-Drop attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-grabbed
+     * @deprecated
+     */
+    public const ARIA_GRABBED = 'aria-grabbed';
+
+
+    /**
+     * HTML aria attribute 'aria-activedescendant'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-activedescendant
+     */
+    public const ARIA_ACTIVEDESCENDANT = 'aria-activedescendant';
+
+
+    /**
+     * HTML aria attribute 'aria-colcount'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-colcount
+     */
+    public const ARIA_COLCOUNT = 'aria-colcount';
+
+
+    /**
+     * HTML aria attribute 'aria-colindex'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-colindex
+     */
+    public const ARIA_COLINDEX = 'aria-colindex';
+
+
+    /**
+     * HTML aria attribute 'aria-colspan'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-colspan
+     */
+    public const ARIA_COLSPAN = 'aria-colspan';
+
+
+    /**
+     * HTML aria attribute 'aria-controls'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-controls
+     */
+    public const ARIA_CONTROLS = 'aria-controls';
+
+
+    /**
+     * HTML aria attribute 'aria-describedby'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-describedby
+     */
+    public const ARIA_DESCRIBEDBY = 'aria-describedby';
+
+
+    /**
+     * HTML aria attribute 'aria-description'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-description
+     */
+    public const ARIA_DESCRIPTION = 'aria-description';
+
+
+    /**
+     * HTML aria attribute 'aria-details'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-details
+     */
+    public const ARIA_DETAILS = 'aria-details';
+
+
+    /**
+     * HTML aria attribute 'aria-flowto'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-flowto
+     */
+    public const ARIA_FLOWTO = 'aria-flowto';
+
+
+    /**
+     * HTML aria attribute 'aria-labelledby'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-labelledby
+     */
+    public const ARIA_LABELLEDBY = 'aria-labelledby';
+
+
+    /**
+     * HTML aria attribute 'aria-owns'
+     *
+     * Type: Relationship attributes, Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-owns
+     */
+    public const ARIA_OWNS = 'aria-owns';
+
+
+    /**
+     * HTML aria attribute 'aria-posinset'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-posinset
+     */
+    public const ARIA_POSINSET = 'aria-posinset';
+
+
+    /**
+     * HTML aria attribute 'aria-rowcount'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-rowcount
+     */
+    public const ARIA_ROWCOUNT = 'aria-rowcount';
+
+
+    /**
+     * HTML aria attribute 'aria-rowindex'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-rowindex
+     */
+    public const ARIA_ROWINDEX = 'aria-rowindex';
+
+
+    /**
+     * HTML aria attribute 'aria-rowspan'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-rowspan
+     */
+    public const ARIA_ROWSPAN = 'aria-rowspan';
+
+
+    /**
+     * HTML aria attribute 'aria-setsize'
+     *
+     * Type: Relationship attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-setsize
+     */
+    public const ARIA_SETSIZE = 'aria-setsize';
+
+
+    /**
+     * HTML aria attribute 'aria-current'
+     *
+     * Type: Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-current
+     */
+    public const ARIA_CURRENT = 'aria-current';
+
+
+    /**
+     * HTML aria attribute 'aria-keyshortcuts'
+     *
+     * Type: Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-keyshortcuts
+     */
+    public const ARIA_KEYSHORTCUTS = 'aria-keyshortcuts';
+
+
+    /**
+     * HTML aria attribute 'aria-roledescription'
+     *
+     * Type: Global attributes
+     *
+     * @var string
+     * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-roledescription
+     */
+    public const ARIA_ROLEDESCRIPTION = 'aria-roledescription';
+}
